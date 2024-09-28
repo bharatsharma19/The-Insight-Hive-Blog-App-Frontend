d31f86ee1da40bdcdcbd07c3adfbcc99b5d28832
Bharat Sharma <82795831+bharatsharma19@users.noreply.github.com>
2024-09-28 17:00:16 +0530

    chore: added type button in auth

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -110,6 +110,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
 
                     <div className="pt-8">
                         <button
+                            type="button"
                             className="bg-gray-700 hover:bg-black text-white font-bold py-2 px-4 rounded-full w-full"
                             onClick={sendRequest}
                             disabled={isLoading}
